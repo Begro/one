e0015193b1ea7c9700b3f18623ed653ebde57f49
liuyan9 <liuyan9@yonyou.com>
2017-06-06 14:27:01 +0800

    '随便加的玩'

--- src/main/java/com/liu/dai/processor/TextProcessor.java
+++ src/main/java/com/liu/dai/processor/TextProcessor.java
@@ -15,12 +15,15 @@ public class TextProcessor extends AbstractProcessor implements IProcessor<InTex
     public void process(InTextMsg inTextMsg, HttpServletRequest request, HttpServletResponse response) {
 
         String content = inTextMsg.getContent();
-        String result = null;
+        String result = "你说啥，我听不懂啊~~~";
         if ("代记婷".equalsIgnoreCase(content)) {
             result = "刘研";
         }
         if ("刘研".equalsIgnoreCase(content)) {
             result = "代记婷";
+        }
+        if("孔令然".equalsIgnoreCase(content)){
+            result ="我叫二哥，江湖人称孔老二，最喜欢爆人菊花";
         }
         TextMsg textMsg = new TextMsg(inTextMsg);
         textMsg.setContent(result);
